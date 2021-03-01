340bb8661ffdc4f6c25eeaac7e908bc8f4b1edf8
Cheng Chang <xcc@fb.com>
2021-03-01 06:42:24 -0800

    export symbols

--- torch/csrc/jit/tensorexpr/external_functions.h
+++ torch/csrc/jit/tensorexpr/external_functions.h
@@ -25,6 +25,33 @@ TORCH_API void nnc_aten_matmul(
     int64_t args_num,
     int64_t* extra_args);
 
+TORCH_API void nnc_aten_adaptive_avg_pool2d(
+    int64_t bufs_num,
+    void** buf_data,
+    int64_t* buf_ranks,
+    int64_t* buf_dims,
+    int8_t* buf_dtypes,
+    int64_t args_num,
+    int64_t* extra_args);
+
+TORCH_API void nnc_aten_mean(
+    int64_t bufs_num,
+    void** buf_data,
+    int64_t* buf_ranks,
+    int64_t* buf_dims,
+    int8_t* buf_dtypes,
+    int64_t args_num,
+    int64_t* extra_args);
+
+TORCH_API void nnc_aten_addmm(
+    int64_t bufs_num,
+    void** buf_data,
+    int64_t* buf_ranks,
+    int64_t* buf_dims,
+    int8_t* buf_dtypes,
+    int64_t args_num,
+    int64_t* extra_args);
+
 } // namespace tensorexpr
 } // namespace jit
 } // namespace torch
